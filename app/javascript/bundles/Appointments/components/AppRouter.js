087babfac1880404d32ac49009963f48e3f8903f
hello <hello@hellos-iMac.local>
2019-02-07 19:32:43 +0100

    Fix router

--- app/javascript/bundles/Appointments/components/AppRouter.js
+++ app/javascript/bundles/Appointments/components/AppRouter.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Appointments from './Appointments';
 import Appointment from './Appointment';
 
 const AppRouter = (props) => {
     return (
         <Router>
-            <div>
+            <Switch>
                 <Route path="/" render={routeProps => (
                     <Appointments {...routeProps} appointments={props.appointments} />
                 )} />
-                <Route path="/appointments/:id" render={routeProps => (
-                    <Appointment {...routeProps} />
-                )} />
-            </div>
+                <Route path="/appointments/:id" component={Appointment} />
+            </Switch>
         </Router>
     );
 }
